39ca3c85b568a4d741bce08859072a6e0e3fd139
herokudev <ao43085@gmail.com>
2021-09-28 15:33:29 -0600

    Fixing linter errors
    Fixing linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
-import Home from './pages/Home';
-import Quote from './pages/Quote';
+import Home from './components/Home';
+import Quote from './components/Quote';
 import './App.css';
 
 const App = () => (
